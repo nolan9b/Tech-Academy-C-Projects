373d69aa373e618d349a0bf4defd8ecd3cfd5a99
nolan <nolan9b@outlook.com>
2018-11-20 22:22:06 +0800

    Commit MVC Changes

--- MVCTutorial/MVCTutorial/Controllers/HomeController.cs
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using MVCTutorial.Models;
 
 namespace MVCTutorial.Controllers
 {
@@ -10,6 +12,37 @@ namespace MVCTutorial.Controllers
     {
         public ActionResult Index()
         {
+            //string text = "hello";
+            //System.IO.File.WriteAllText(@"D:\logs\allusers.txt", text);
+
+            //var rnd = new Random(10);
+            //int num = rnd.Next();
+
+
+            //ViewBag.RandomNumber = num;
+
+            // return View("About");
+
+            // return Content("Hello");
+
+            // return RedirectToAction("Contact");
+
+
+            var names = new List<string>()
+            {
+                "Peter",
+                "Pan",
+                "Wendy"
+            };
+
+            var user = new User();
+            user.Id = 1;
+            user.FirstName = "Peter";
+            user.LastName = "Pan";
+            user.Age = 32;
+
+            return View(user);
+
             return View();
         }
 
---
+++ TechAcaStudentsMVC/TechAcaStudentsMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechAcaStudentsMVC.MvcApplication" Language="C#" %>
