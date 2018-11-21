71e56fc839f71a32fc3caeeafd64c5ca06eb517f
nolan <nolan9b@outlook.com>
2018-11-21 22:26:44 +0800

    Commit Newsletter Initial Draft

--- StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -9,6 +9,9 @@ namespace StudentManagementSystem.Controllers
     public class StudentController : Controller
     {
         // GET: Student
+        private string _connectionString = @"Data Source=NOLAN\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect 
+                                            Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         public ActionResult Index()
         {
             return View();
