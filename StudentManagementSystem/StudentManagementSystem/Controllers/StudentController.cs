fd27bf0a8c14859e972668ddc97cca1b4afd9430
nolan <nolan9b@outlook.com>
2018-12-03 09:58:28 +0800

    Finished Student Management and Newsletter MVC Apps

--- StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using StudentManagementSystem.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,12 +12,142 @@ namespace StudentManagementSystem.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-        private string _connectionString = @"Data Source=NOLAN\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect 
-                                            Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private string _connectionString = @"Data Source = NOLAN\SQLEXPRESS;Initial Catalog = School; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        
 
         public ActionResult Index()
         {
+            string queryString = "Select * From Students";
+            var students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var student = new Student();
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                    students.Add(student);
+                }
+                connection.Close();
+            }
+                return View(students);
+        }
+
+        public ActionResult Add()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Add(Student student)
+        {
+            string queryString = @"Insert into Students (FirstName, LastName) Values (@FirstName, @LastName)";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+
+                command.Parameters["@FirstName"].Value = student.FirstName;
+                command.Parameters["@LastName"].Value = student.LastName;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            string queryString = "Select * From Students where id = @id";
+
+            var student = new Student();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@id", SqlDbType.Int);
+                
+                command.Parameters["@id"].Value = id;
+                
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                }
+
+                connection.Close();
+            }
+            return View(student);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            string queryString = "Select * From Students where id = @id";
+
+            var student = new Student();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@id", SqlDbType.Int);
+
+                command.Parameters["@id"].Value = id;
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                }
+
+                connection.Close();
+            }
+            return View(student);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            string queryString = @"Update Students set FirstName = @FirstName, LastName = @LastName where Id = @id";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@Id", SqlDbType.Int);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+
+                command.Parameters["@Id"].Value = student.Id;
+                command.Parameters["@FirstName"].Value = student.FirstName;
+                command.Parameters["@LastName"].Value = student.LastName;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
